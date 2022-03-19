e1bbad5256309a05b2258d98fa2b077057813153
Jiří Vrba <jiri@vrba.dev>
2022-03-19 16:56:26 +0100

    Discord configuration properties

--- src/main/kotlin/dev/vrba/dubsbot/DubsBotApplication.kt
+++ src/main/kotlin/dev/vrba/dubsbot/DubsBotApplication.kt
@@ -1,8 +1,10 @@
 package dev.vrba.dubsbot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan(basePackages = ["dev.vrba.dubsbot.configuration"])
 @SpringBootApplication
 class DubsBotApplication
 
