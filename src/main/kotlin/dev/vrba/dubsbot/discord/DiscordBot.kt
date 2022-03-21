ba2b01c4b6a6ad4ad43c098c1c7e98fe4088de43
Jiří Vrba <jiri@vrba.dev>
2022-03-21 08:26:27 +0100

    Discord bot startup class definition

---
+++ src/main/kotlin/dev/vrba/dubsbot/discord/DiscordBot.kt
@@ -0,0 +1,68 @@
+package dev.vrba.dubsbot.discord
+
+import dev.vrba.dubsbot.configuration.DiscordConfiguration
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.OnlineStatus
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.hooks.EventListener
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import org.springframework.boot.CommandLineRunner
+import org.springframework.core.env.Environment
+import org.springframework.core.env.Profiles
+import org.springframework.stereotype.Component
+
+@Component
+class DiscordBot(
+    private val configuration: DiscordConfiguration,
+    private val environment: Environment,
+    private val listeners: List<EventListener>,
+    private val commands: List<SlashCommand>
+) : CommandLineRunner {
+
+    private val client: JDA = JDABuilder.createDefault(configuration.token)
+        .build()
+        .awaitReady()
+
+    override fun run(vararg args: String) {
+        registerEventListeners()
+        registerSlashCommands()
+        updateBotStatus()
+    }
+
+    private fun registerEventListeners() {
+        listeners.forEach {
+            client.addEventListener(it)
+        }
+    }
+
+    private fun registerSlashCommands() {
+        val handler = object : ListenerAdapter() {
+            override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+                // TODO: Add exception handling
+                commands.firstOrNull { it.define().name == event.name }?.handle(event)
+            }
+        }
+
+        client.addEventListener(handler)
+
+        val commands = commands.map { it.define() }
+
+        if (environment.acceptsProfiles(Profiles.of("development"))) {
+            val guild = client.getGuildById(configuration.developmentGuildId) ?: throw IllegalStateException("Cannot find the configured guild")
+
+            return guild.updateCommands()
+                .addCommands(commands)
+                .queue()
+        }
+
+        return client.updateCommands()
+            .addCommands(commands)
+            .queue()
+    }
+
+    private fun updateBotStatus() {
+        client.presence.activity = Activity.watching("for dubs")
+    }
+}
---
+++ src/main/kotlin/dev/vrba/dubsbot/discord/SlashCommand.kt
@@ -0,0 +1,12 @@
+package dev.vrba.dubsbot.discord
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
+
+interface SlashCommand {
+
+    fun define(): SlashCommandData
+
+    fun handle(event: SlashCommandInteractionEvent)
+
+}
