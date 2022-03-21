ef751ef34fd4b76e415814a42e75aabf8f0afece
Jiří Vrba <jiri@vrba.dev>
2022-03-21 07:57:09 +0100

    Add guild leaderboard method to the ScoresRepository

--- src/main/kotlin/dev/vrba/dubsbot/repositories/ScoresRepository.kt
+++ src/main/kotlin/dev/vrba/dubsbot/repositories/ScoresRepository.kt
@@ -1,9 +1,20 @@
 package dev.vrba.dubsbot.repositories
 
 import dev.vrba.dubsbot.entities.Score
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface ScoresRepository : CrudRepository<Score, UUID>
+interface ScoresRepository : CrudRepository<Score, UUID> {
+
+    fun findByUserIdAndGuildId(userId: Long, guildId: Long): Score?
+
+    @Query("""
+       select new Score(id, userId, guildId, dubs, trips, quads, pents) from Score
+       order by pents desc, quads desc, trips desc, dubs desc
+    """)
+    fun guildLeaderboard(guildId: Long): List<Score>
+
+}
