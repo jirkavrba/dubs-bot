a7b9eeb2100fe334122a3b0ad5083d4d4c24f078
Jiří Vrba <jiri@vrba.dev>
2022-07-23 16:55:50 +0200

    Add tests covering basic dubs lookup

--- src/main/kotlin/dev/vrba/dubsbot/services/DubsLookupService.kt
+++ src/main/kotlin/dev/vrba/dubsbot/services/DubsLookupService.kt
@@ -2,19 +2,18 @@ package dev.vrba.dubsbot.services
 
 import dev.vrba.dubsbot.domain.*
 import org.springframework.stereotype.Service
-import kotlin.math.sqrt
 
 @Service
 class DubsLookupService {
 
-    fun findMatches(message: Long, mode: DubsLookupMode): List<DubsMatch> {
+    fun findMatches(message: Long, mode: DubsLookupMode): Set<DubsMatch> {
         val basic = findBasicMatches(message)
 
         if (mode == DubsLookupMode.Basic) {
             return basic
         }
 
-        return basic + listOfNotNull(
+        return basic + setOfNotNull(
                 findConsMatch(message),
                 findOffByOneMatch(message),
                 findPalindromeMatch(message),
@@ -22,9 +21,9 @@ class DubsLookupService {
         )
     }
 
-    private tailrec fun findBasicMatches(message: Long, matches: List<DubsMatch> = emptyList()): List<DubsMatch> {
+    private tailrec fun findBasicMatches(message: Long, matches: Set<DubsMatch> = emptySet()): Set<DubsMatch> {
         // A single digit cannot be matched
-        if (message <= 10) return emptyList()
+        if (message <= 10) return emptySet()
 
         val reversed = message.toString().reversed()
         val first = reversed.first()
--- src/test/kotlin/dev/vrba/dubsbot/services/DubsLookupServiceTests.kt
+++ src/test/kotlin/dev/vrba/dubsbot/services/DubsLookupServiceTests.kt
@@ -1,5 +1,6 @@
 package dev.vrba.dubsbot.services
 
+import dev.vrba.dubsbot.domain.BasicMatch
 import dev.vrba.dubsbot.domain.DubsLookupMode
 import dev.vrba.dubsbot.domain.PrimeMatch
 import org.junit.jupiter.api.Test
@@ -10,17 +11,56 @@ class DubsLookupServiceTests {
     private val service = DubsLookupService()
 
     @Test
+    fun `test basic matches lookup`() {
+        assertTrue(service.findMatches(4346075790683833901, DubsLookupMode.Basic).isEmpty())
+
+        assertTrue(service.findMatches(4346075790683833900, DubsLookupMode.Basic).let {
+            it.isNotEmpty() &&
+                    it.filterIsInstance<BasicMatch>().size == 1 &&
+                    it.filterIsInstance<BasicMatch>().filter { match -> match.digits == 2 }.size == 1
+        })
+
+        assertTrue(service.findMatches(4346075790683839000, DubsLookupMode.Basic).let {
+            it.isNotEmpty() &&
+                    it.size == 1 &&
+                    it.filterIsInstance<BasicMatch>().size == 1 &&
+                    it.filterIsInstance<BasicMatch>().filter { match -> match.digits == 3 }.size == 1
+        })
+
+        assertTrue(service.findMatches(4346075790683830000, DubsLookupMode.Basic).let {
+            it.isNotEmpty() &&
+                    it.size == 1 &&
+                    it.filterIsInstance<BasicMatch>().size == 1 &&
+                    it.filterIsInstance<BasicMatch>().filter { match -> match.digits == 4 }.size == 1
+        })
+
+        assertTrue(service.findMatches(4346075790883833300, DubsLookupMode.Basic).let {
+            it.isNotEmpty() &&
+                    it.size == 2 &&
+                    it.filterIsInstance<BasicMatch>().size == 2 &&
+                    it.filterIsInstance<BasicMatch>().filter { match -> match.digits == 2 }.size == 1 &&
+                    it.filterIsInstance<BasicMatch>().filter { match -> match.digits == 3 }.size == 1
+        })
+
+        assertTrue(service.findMatches(4346075798888800000, DubsLookupMode.Basic).let {
+            it.isNotEmpty() &&
+                    it.size == 2 &&
+                    it.filterIsInstance<BasicMatch>().size == 2 &&
+                    it.filterIsInstance<BasicMatch>().filter { match -> match.digits == 5 }.size == 2
+        })
+    }
+
+
+    @Test
     fun `test prime matches lookup`() {
-        assertTrue(
-                service.findMatches(4346075790683833919, DubsLookupMode.Extended)
-                        .filterIsInstance<PrimeMatch>()
-                        .count() == 1
+        assertTrue(service.findMatches(4346075790683833919, DubsLookupMode.Extended)
+                .filterIsInstance<PrimeMatch>()
+                .count() == 1
         )
 
-        assertTrue(
-                service.findMatches(4346075790683833912, DubsLookupMode.Extended)
-                        .filterIsInstance<PrimeMatch>()
-                        .isEmpty()
+        assertTrue(service.findMatches(4346075790683833912, DubsLookupMode.Extended)
+                .filterIsInstance<PrimeMatch>()
+                .isEmpty()
         )
     }
 }
