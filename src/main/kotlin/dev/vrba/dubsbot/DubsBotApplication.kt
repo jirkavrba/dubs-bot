a382a2b9dd71b03ed5375ff1ff60acd9208e1fb9
Jiří Vrba <jiri@vrba.dev>
2022-07-23 20:01:56 +0200

    Add discord configuration properties

--- src/main/kotlin/dev/vrba/dubsbot/DubsBotApplication.kt
+++ src/main/kotlin/dev/vrba/dubsbot/DubsBotApplication.kt
@@ -1,9 +1,12 @@
 package dev.vrba.dubsbot
 
+import dev.vrba.dubsbot.configuration.DiscordConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(DiscordConfiguration::class)
 class DubsBotApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/dev/vrba/dubsbot/configuration/DiscordConfiguration.kt
@@ -0,0 +1,11 @@
+package dev.vrba.dubsbot.configuration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "discord")
+data class DiscordConfiguration(
+    val token: String,
+    val developmentGuildId: Long
+)
