150c1537f3eb118b73387f5d99804d05ee199fbb
Jiří Vrba <jiri@vrba.dev>
2022-07-23 14:20:31 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "dubs-bot"
---
+++ src/test/kotlin/dev/vrba/dubsbot/DubsBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.vrba.dubsbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DubsBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
