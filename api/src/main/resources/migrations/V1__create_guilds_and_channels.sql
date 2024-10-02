d35a7d75b19da39255dae6069d1ba49d1f01c5da
Jiří Vrba <jiri@vrba.dev>
2024-10-02 22:00:53 +0200

    Add migrations for users, guilds and channels

---
+++ api/src/main/resources/migrations/V1__create_guilds_and_channels.sql
@@ -0,0 +1,17 @@
+create table if not exists guilds
+(
+    id       varchar(32)  not null primary key,
+    name     varchar(128) not null,
+    icon_url varchar(256) not null
+);
+
+create table if not exists channels
+(
+    id       varchar(32)  not null primary key,
+    name     varchar(128) not null,
+    guild_id varchar(32)  not null,
+
+    constraint fk_channels_guild_id foreign key (guild_id) references channels (id) on delete cascade
+);
+
+create index on channels (guild_id);
