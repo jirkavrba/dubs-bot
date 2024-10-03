fc5b50c4ed34b94a9cea85549c0dbcb8c67d1c84
Jiří Vrba <jiri@vrba.dev>
2024-10-03 22:40:03 +0200

    Implement matching primes + actually calling patterns in matching service

--- api/src/main/java/dev/vrba/dubs/service/PatternMatchingService.java
+++ api/src/main/java/dev/vrba/dubs/service/PatternMatchingService.java
@@ -3,14 +3,21 @@ package dev.vrba.dubs.service;
 import dev.vrba.dubs.domain.Pattern;
 import io.micronaut.core.annotation.NonNull;
 import jakarta.inject.Singleton;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Singleton
+@RequiredArgsConstructor
 public class PatternMatchingService {
 
     @NonNull
+    private final List<Pattern> patterns;
+
+    @NonNull
     public List<Pattern> matchMessagePatterns(@NonNull String id) {
-        return List.of();
+        return patterns.stream()
+                .filter(pattern -> pattern.matches(id))
+                .toList();
     }
 }
---
+++ api/src/main/java/dev/vrba/dubs/service/pattern/PrimesPattern.java
@@ -0,0 +1,35 @@
+package dev.vrba.dubs.service.pattern;
+
+import dev.vrba.dubs.domain.Pattern;
+import io.micronaut.core.annotation.NonNull;
+import jakarta.inject.Singleton;
+
+import java.math.BigInteger;
+
+@Singleton
+public class PrimesPattern implements Pattern {
+    @Override
+    public String getName() {
+        return "primes";
+    }
+
+    @Override
+    public String getEmoji() {
+        return ":nerd:";
+    }
+
+    @Override
+    public long getPoints() {
+        return 100;
+    }
+
+    @Override
+    public boolean isRare() {
+        return false;
+    }
+
+    @Override
+    public boolean matches(@NonNull String id) {
+        return new BigInteger(id).isProbablePrime(20);
+    }
+}
